4d7766c98d8be36ecd503fbeef6c99ba40c4eb67
vacchanidhavalMahendar <75673729+vacchanidhavalMahendar@users.noreply.github.com>
2021-05-30 13:45:40 +0530

    Add files via upload

---
+++ UNIT10/01_oops.py
@@ -0,0 +1,22 @@
+class Number:
+    def sum(self):
+        return self.a + self.b
+
+num = Number()
+num.a = 55
+num.b = 66
+s = num.sum()
+print(s)
+
+# a = 55
+# b = 66
+
+# print("The sum of a and b is ", a+b)
+
+'''
+PascalCase 
+EmployeeName -->PascalCase 
+
+camelCase
+isNumeric, isFloatOrInt -->camelCase
+'''
