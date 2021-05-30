0b3db417203070f6816ca8f345ebf66d2c9c7086
vacchanidhavalMahendar <75673729+vacchanidhavalMahendar@users.noreply.github.com>
2021-05-30 13:35:30 +0530

    Add files via upload

---
+++ UNIT6/01_conditional.py
@@ -0,0 +1,35 @@
+# if elif else statement in ladder in python
+a = 45
+if a >3:
+    print("The value of a is greater than 3")
+elif a >7:
+    print("The value of a is greater than 7")
+elif a >40:
+    print("The value is not greater than 3 or 7")
+
+
+b = 15
+if b <3:
+    print("The value of b is greater than 3")
+elif b >7:
+    print("The value of b is greater than 10")
+elif b > 40:
+    print("The value is not greater than 3 or 7")
+
+
+#multiple if statements in python.
+if(a<3): 
+    print("The value of a is greater than 3")
+
+if(a>13):
+    print("The value of a is greater than 13")
+    
+if(a>7):
+    print("The value of a is greater than 7")
+
+if(a>17):
+    print("The value of a is greater than 17")
+else:
+    print("The value is not greater than 3 or 7")
+
+print("Done")
